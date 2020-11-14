eb77440a9b449dc98449df9d35f5c2d7d64bf10d
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2020-11-14 10:50:32 +0800

    Update Desktop Tidiness Helper.cpp

--- Desktop Tidiness Helper.cpp
+++ Desktop Tidiness Helper.cpp
@@ -6,83 +6,42 @@
 #include "framework.h"
 #include <stdio.h>
 #include <Dbt.h>
+#include <ShlObj.h>
+#include <time.h>
+
+#define MAX_PATH_LENGTH 260
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[] = L"DTH";                       // The title bar text
 WCHAR szWindowClass[] = L"My Window Class";     // the main window class name
-WCHAR szHomePath[128], szLogfilePath[128], szConfigfilePath[128], szQueuefilePath[128];
+WCHAR szHomePath[MAX_PATH_LENGTH], szLogfilePath[MAX_PATH_LENGTH], szConfigfilePath[MAX_PATH_LENGTH], szQueuefilePath[MAX_PATH_LENGTH], szDesktopPath[MAX_PATH_LENGTH];
 FILE* fpLogfile, * fpConfigfile, * fpQueuefile;
 
 // Structs
 struct DRIVE {
     DWORD uuid = 0;
-    WCHAR path[128] = L"";
+    WCHAR path[MAX_PATH_LENGTH] = L"";
     DRIVE* pnext = nullptr;
 } driveHead;
-
 struct EXEMPT {
-    WCHAR name[128] = L"";
+    WCHAR name[MAX_PATH_LENGTH] = L"";
     EXEMPT* pnext = nullptr;
 } exemptHead;
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
-{
-    // Initialize global strings
-    MyRegisterClass(hInstance);
-
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-
-    MSG msg;
-
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return (int) msg.wParam;
-}
-
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = nullptr;
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = nullptr;
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = nullptr;
-
-    return RegisterClassExW(&wcex);
+const LPWSTR CurTime() {
+    time_t t = time(nullptr);
+    tm* ct = localtime(&t);
+    static WCHAR szt[32] = L"";
+    swprintf(szt, 32, L"%02d-%02d %02d:%02d:%02d", ct->tm_mon, ct->tm_mday, ct->tm_hour, ct->tm_min, ct->tm_sec);
+    delete ct;
+    return szt;
 }
 
 inline void DeleteQueue() {
     fpQueuefile = _wfopen(szQueuefilePath, L"r");
     if (!fpQueuefile) return;
-    WCHAR fname[128] = L"";
+    WCHAR fname[MAX_PATH_LENGTH] = L"";
     while (fwscanf(fpQueuefile, L"%ws", fname) != -1)
     {
         DeleteFile(fname);
@@ -125,7 +84,7 @@ inline void ReadConfig() {
             }
 
             // Config row
-            WCHAR key[128] = L"", value[128] = L"";
+            WCHAR key[MAX_PATH_LENGTH] = L"", value[MAX_PATH_LENGTH] = L"";
             int p = 0;
             while (line[p] != L'=' && p < len) p++;
             if (p == len) {
@@ -165,6 +124,26 @@ inline void ReadConfig() {
     }
 }
 
+DWORD WINAPI Monitor(LPVOID lpParameter) {
+    HANDLE hDirectory = CreateFile(szDesktopPath, FILE_LIST_DIRECTORY, FILE_SHARE_READ ,NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+    DWORD Buf[1024], dwRet;
+    BOOL bRet;
+    FILE_NOTIFY_INFORMATION* pFileNotifyInfo = (FILE_NOTIFY_INFORMATION*)&Buf;
+
+    do
+    {
+        RtlZeroMemory(pFileNotifyInfo, 1024);
+        bRet = ReadDirectoryChangesW(hDirectory, pFileNotifyInfo, 1024, TRUE,
+            FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_LAST_WRITE,
+            &dwRet, NULL, NULL);
+    } while (bRet);
+    return 0;
+}
+
+void SaveLog(HWND hWnd, UINT timerMsg, UINT_PTR tId, DWORD t) {
+    fflush(fpLogfile);
+}
+
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
     HWND hWnd;
@@ -189,6 +168,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     // Init Log
     wsprintf(szLogfilePath, L"%ws\\main.log", szHomePath);
     fpLogfile = _wfopen(szLogfilePath, L"a");
+    fwprintf(fpLogfile, L"[%ws]Program start\n", CurTime());
 
     // Init Config
     wsprintf(szConfigfilePath, L"%ws\\config.ini", szHomePath);
@@ -201,54 +181,82 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     DeleteQueue();
     fpQueuefile = _wfopen(szQueuefilePath, L"w");
 
-    // Init Timer
-    SetTimer(hWnd, 1, 3000, nullptr);
+    // Init Desktop
+    LPITEMIDLIST pidl;
+    LPMALLOC pShellMalloc; 
+    if (SUCCEEDED(SHGetMalloc(&pShellMalloc))) {
+        if (SUCCEEDED(SHGetSpecialFolderLocation(NULL, CSIDL_DESKTOP, &pidl))) {
+            SHGetPathFromIDList(pidl, szDesktopPath);
+            pShellMalloc->Free(pidl);
+        }
+        pShellMalloc->Release();
+    }
+
+    HANDLE hMonitor = CreateThread(nullptr, 0, Monitor, nullptr, 0, nullptr);
+
+    SetTimer(hWnd, 0, 5000, SaveLog);
 
     return TRUE;
 }
 
-inline char FirstDriveFromMask(ULONG unitmask) {
-    char i;
-    for (i = 0; i < 26; ++i) {
-        if (unitmask & 0x1) break;
-        unitmask >>= 1;
-    }
-    return i + 'A';
+void ExitInstance() {
+    fclose(fpConfigfile);
+    fclose(fpQueuefile);
+    fwprintf(fpLogfile, L"[%ws]Program Stopped", CurTime());
+    fclose(fpLogfile);
+}
+
+DWORD WINAPI Indexer(LPVOID lpParameter) {
+    LPWSTR volumeLetter = (LPWSTR)lpParameter;
+    return 0;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
-    case WM_TIMER: {
-        //detect_file_moves();
-        break; 
-    }
     case WM_DESTROY: {
+        ExitInstance();
         PostQuitMessage(0);
         break;
     }
+                   // Device Change
     case WM_DEVICECHANGE: {
         PDEV_BROADCAST_HDR lpdb = (PDEV_BROADCAST_HDR)lParam;
         PDEV_BROADCAST_VOLUME lpdbv = (PDEV_BROADCAST_VOLUME)lpdb;
-        WCHAR szMsg[80], volumeLetter[] = L"C:";
+        WCHAR volumeLetter[] = L"C:";
+        DWORD unitmask = lpdbv->dbcv_unitmask;
         switch (wParam)
         {
         case DBT_DEVICEARRIVAL: {
-            volumeLetter[0] = FirstDriveFromMask(lpdbv->dbcv_unitmask);
-            WCHAR volumeName[128] = L"";
-            DWORD serialNumber = 0;
-            GetVolumeInformation(volumeLetter, volumeName, 128, &serialNumber, nullptr, nullptr, nullptr, 0);
-            DRIVE* p = driveHead.pnext;
-            while (p && p->uuid != serialNumber) p = p->pnext;
-            if (!p) {
-                fwprintf(fpConfigfile, L"\n[%d]\nMovePath = \"\"\n", serialNumber);
-                fpConfigfile = _wfreopen(szConfigfilePath, L"a", fpConfigfile);
+            for (char i = 0; i < 26; ++i) {
+                if (!(unitmask & 0x1)) continue;
+                volumeLetter[0] = i + 'A';
+                WCHAR volumeName[MAX_PATH_LENGTH] = L"";
+                DWORD serialNumber = 0;
+                GetVolumeInformation(volumeLetter, volumeName, MAX_PATH_LENGTH, &serialNumber, nullptr, nullptr, nullptr, 0);
+                fwprintf(fpLogfile, L"[%ws]Drive arrival, VolumeLetter=\"%ws\" Serial=%d, VolumeName=\"%ws\"\n", CurTime(), volumeLetter, serialNumber, volumeName);
+                DRIVE* p = driveHead.pnext;
+                while (p && p->uuid != serialNumber) p = p->pnext;
+                if (!p) {
+                    fwprintf(fpConfigfile, L"\n[%d]\n# VolumeName = %ws\nMovePath = \"\"\n", serialNumber, volumeName);
+                    fflush(fpConfigfile);
+                }
+                else {
+                    if (p->path[0] == L'\0') continue;
+                    CreateThread(nullptr, 0, Indexer, volumeLetter, 0, nullptr);
+                }
+                unitmask >>= 1;
             }
             break;
         }
         case DBT_DEVICEREMOVECOMPLETE: {
-            volumeLetter[0] = FirstDriveFromMask(lpdbv->dbcv_unitmask);
+            for (char i = 0; i < 26; ++i) {
+                if (!(unitmask & 0x1)) continue;
+                volumeLetter[0] = i + 'A';
+                fwprintf(fpLogfile, L"[%ws]Drive remove, VolumeLetter=\"%ws\"\n", CurTime(), volumeLetter);
+                unitmask >>= 1;
+            }
             break;
         }
         default: {
@@ -262,4 +270,52 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     }
     }
     return 0;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = nullptr;
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = nullptr;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = nullptr;
+
+    return RegisterClassExW(&wcex);
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    // Initialize global strings
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
 }
