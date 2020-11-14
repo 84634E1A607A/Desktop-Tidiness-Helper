ed75bf21c08f68f5e143bb0accb95a4cdb56bb91
84634E1A607A <60734649+84634E1A607A@users.noreply.github.com>
2020-11-14 15:05:36 +0800

    Finished

--- Desktop Tidiness Helper.cpp
+++ Desktop Tidiness Helper.cpp
@@ -1,5 +1,3 @@
-// Desktop Tidiness Helper.cpp : Defines the entry point for the application.
-//
 #define _CRT_SECURE_NO_WARNINGS
 
 
@@ -8,24 +6,41 @@
 #include <Dbt.h>
 #include <ShlObj.h>
 #include <time.h>
+#include <mutex>
+#include <vector>
+#include <algorithm>
 
-#define MAX_PATH_LENGTH 260
+using namespace std;
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[] = L"DTH";                       // The title bar text
 WCHAR szWindowClass[] = L"My Window Class";     // the main window class name
-WCHAR szHomePath[MAX_PATH_LENGTH], szLogfilePath[MAX_PATH_LENGTH], szConfigfilePath[MAX_PATH_LENGTH], szQueuefilePath[MAX_PATH_LENGTH], szDesktopPath[MAX_PATH_LENGTH];
-FILE* fpLogfile, * fpConfigfile, * fpQueuefile;
+WCHAR szHomePath[MAX_PATH], szConfigfilePath[MAX_PATH], szQueuefilePath[MAX_PATH], szDesktopPath[MAX_PATH];
+FILE* fpConfigfile, * fpQueuefile;
 
 // Structs
+struct FILEINFO {
+    WCHAR name[MAX_PATH] = L"";
+    DWORD size = 0;
+    FILEINFO(LPWSTR n, DWORD s) { lstrcpyW(name, n); size = s; }
+    FILEINFO() {}
+    bool operator< (const FILEINFO r) const {
+        return lstrcmpW(this->name, r.name) < 0;
+    }
+    bool operator== (const FILEINFO r) const {
+        return !lstrcmpW(this->name, r.name) && this->size == r.size;
+    }
+};
 struct DRIVE {
     DWORD uuid = 0;
-    WCHAR path[MAX_PATH_LENGTH] = L"";
+    WCHAR path[MAX_PATH] = L"";
+    WCHAR letter[3] = L"";
+    vector<FILEINFO> files;
     DRIVE* pnext = nullptr;
 } driveHead;
 struct EXEMPT {
-    WCHAR name[MAX_PATH_LENGTH] = L"";
+    WCHAR name[MAX_PATH] = L"";
     EXEMPT* pnext = nullptr;
 } exemptHead;
 
@@ -34,18 +49,15 @@ const LPWSTR CurTime() {
     tm* ct = localtime(&t);
     static WCHAR szt[32] = L"";
     swprintf(szt, 32, L"%02d-%02d %02d:%02d:%02d", ct->tm_mon, ct->tm_mday, ct->tm_hour, ct->tm_min, ct->tm_sec);
-    delete ct;
     return szt;
 }
 
 inline void DeleteQueue() {
-    fpQueuefile = _wfopen(szQueuefilePath, L"r");
+    fpQueuefile = _wfopen(szQueuefilePath, L"rb");
     if (!fpQueuefile) return;
-    WCHAR fname[MAX_PATH_LENGTH] = L"";
-    while (fwscanf(fpQueuefile, L"%ws", fname) != -1)
-    {
+    WCHAR fname[MAX_PATH] = L"";
+    while (fread(fname, MAX_PATH, 1, fpQueuefile))
         DeleteFile(fname);
-    }
     fclose(fpQueuefile);
 }
 
@@ -84,21 +96,15 @@ inline void ReadConfig() {
             }
 
             // Config row
-            WCHAR key[MAX_PATH_LENGTH] = L"", value[MAX_PATH_LENGTH] = L"";
+            WCHAR key[MAX_PATH] = L"", value[MAX_PATH] = L"";
             int p = 0;
             while (line[p] != L'=' && p < len) p++;
-            if (p == len) {
-                fwprintf(fpLogfile, L"Error in config file: %ws\n", line);
-                continue;
-            }
+            if (p == len) continue;
             memcpy(key, line, p * sizeof(WCHAR)), trim(key);
             lstrcpyW(value, line + p + 1), trim(value);
             
             if (!lstrcmpW(key, L"MovePath")) {
-                if (!driveHead.pnext || driveHead.pnext->path[0] != L'\0') {
-                    fwprintf(fpLogfile, L"Error in config file: %ws\n", line);
-                    continue;
-                }
+                if (!driveHead.pnext || driveHead.pnext->path[0] != L'\0') continue;
                 if (value[0] == L'\"') { 
                     value[lstrlenW(value) - 1] = L'\0';
                     lstrcpyW(driveHead.pnext->path, value + 1);
@@ -125,23 +131,62 @@ inline void ReadConfig() {
 }
 
 DWORD WINAPI Monitor(LPVOID lpParameter) {
-    HANDLE hDirectory = CreateFile(szDesktopPath, FILE_LIST_DIRECTORY, FILE_SHARE_READ ,NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
-    DWORD Buf[1024], dwRet;
-    BOOL bRet;
-    FILE_NOTIFY_INFORMATION* pFileNotifyInfo = (FILE_NOTIFY_INFORMATION*)&Buf;
-
-    do
-    {
-        RtlZeroMemory(pFileNotifyInfo, 1024);
-        bRet = ReadDirectoryChangesW(hDirectory, pFileNotifyInfo, 1024, TRUE,
-            FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_LAST_WRITE,
+    HANDLE hDirectory = CreateFile(szDesktopPath, FILE_LIST_DIRECTORY, 
+        FILE_SHARE_READ | FILE_SHARE_DELETE | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+    DWORD* Buf = new DWORD[8192], *Buf2 = new DWORD[1024], dwRet;
+    FILE_NOTIFY_INFORMATION* pFirstFileNotifyInfo = (FILE_NOTIFY_INFORMATION*)Buf, *pFileNotifyInfo = (FILE_NOTIFY_INFORMATION*)Buf2;
+    while (true) {
+        RtlZeroMemory(pFirstFileNotifyInfo, 8192);
+        ReadDirectoryChangesW(hDirectory, pFirstFileNotifyInfo, 8192, FALSE,
+            FILE_NOTIFY_CHANGE_LAST_ACCESS,
             &dwRet, NULL, NULL);
-    } while (bRet);
-    return 0;
-}
+        if (pFirstFileNotifyInfo->Action == FILE_ACTION_MODIFIED) {
+            do {
+                RtlZeroMemory(pFileNotifyInfo, 1024);
+                memcpy(pFileNotifyInfo, pFirstFileNotifyInfo, pFirstFileNotifyInfo->NextEntryOffset ? pFirstFileNotifyInfo->NextEntryOffset - 1 : 1024);
+                pFirstFileNotifyInfo = (FILE_NOTIFY_INFORMATION*)((BYTE*)pFirstFileNotifyInfo + pFirstFileNotifyInfo->NextEntryOffset);
+                if (!lstrcmpW(pFileNotifyInfo->FileName + pFileNotifyInfo->FileNameLength - 4, L".lnk")) continue;
+                WCHAR fname[MAX_PATH];
+                swprintf(fname, MAX_PATH, L"%ws\\%ws", szDesktopPath, pFileNotifyInfo->FileName);
+                WIN32_FIND_DATAW fdata;
+                FindClose(FindFirstFile(fname, &fdata));
+                FILEINFO fInfo = { fdata.cFileName, fdata.nFileSizeLow };
+                DRIVE* p = driveHead.pnext;
+                while (p) {
+                    auto pos = find(p->files.begin(), p->files.end(), fInfo);
+                    if (pos != p->files.end()) break;
+                    p = p->pnext;
+                }
+                if (!p) continue;
+                WCHAR szMovedName[MAX_PATH];
+                swprintf(szMovedName, MAX_PATH, L"%ws\\%ws\\%ws", szDesktopPath, p->path, pFileNotifyInfo->FileName);
 
-void SaveLog(HWND hWnd, UINT timerMsg, UINT_PTR tId, DWORD t) {
-    fflush(fpLogfile);
+                if (!MoveFile(fname, szMovedName)) continue;
+
+                WCHAR szShortcutName[MAX_PATH];
+                swprintf(szShortcutName, MAX_PATH, L"%ws.lnk", fname);
+                IShellLink* psl;
+                if (!SUCCEEDED(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE))) continue;
+                HRESULT hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&psl));
+                if (SUCCEEDED(hr))
+                {
+                    psl->SetPath(szMovedName);
+                    IPersistFile* ppf;
+                    hr = psl->QueryInterface(&ppf);
+                    if (SUCCEEDED(hr)) {
+                        hr = ppf->Save(szShortcutName, TRUE);
+                        ppf->Release();
+                    }
+                    psl->Release();
+                }
+                CoUninitialize();
+                fwrite(szShortcutName, MAX_PATH, 1, fpQueuefile);
+                fflush(fpQueuefile);
+            } while (pFileNotifyInfo->NextEntryOffset);
+        }
+    }
+    delete[] Buf, Buf2;
+    return 0;
 }
 
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
@@ -165,11 +210,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     if (attr == -1 || !(attr & FILE_ATTRIBUTE_DIRECTORY))
         CreateDirectory(szHomePath, NULL);
 
-    // Init Log
-    wsprintf(szLogfilePath, L"%ws\\main.log", szHomePath);
-    fpLogfile = _wfopen(szLogfilePath, L"a");
-    fwprintf(fpLogfile, L"[%ws]Program start\n", CurTime());
-
     // Init Config
     wsprintf(szConfigfilePath, L"%ws\\config.ini", szHomePath);
     fpConfigfile = _wfopen(szConfigfilePath, L"r");
@@ -179,9 +219,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     // Init Queue
     wsprintf(szQueuefilePath, L"%ws\\queue", szHomePath);
     DeleteQueue();
-    fpQueuefile = _wfopen(szQueuefilePath, L"w");
+    fpQueuefile = _wfopen(szQueuefilePath, L"wb");
 
-    // Init Desktop
+    // Init Desktop Path
     LPITEMIDLIST pidl;
     LPMALLOC pShellMalloc; 
     if (SUCCEEDED(SHGetMalloc(&pShellMalloc))) {
@@ -194,20 +234,41 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
     HANDLE hMonitor = CreateThread(nullptr, 0, Monitor, nullptr, 0, nullptr);
 
-    SetTimer(hWnd, 0, 5000, SaveLog);
-
     return TRUE;
 }
 
 void ExitInstance() {
     fclose(fpConfigfile);
     fclose(fpQueuefile);
-    fwprintf(fpLogfile, L"[%ws]Program Stopped", CurTime());
-    fclose(fpLogfile);
+}
+
+vector<FILEINFO> IndexerWorker(LPWSTR dir) {
+    vector<FILEINFO> fInfo;
+    WIN32_FIND_DATAW findData = {};
+    WCHAR szFindCommand[MAX_PATH];
+    swprintf(szFindCommand, MAX_PATH, L"%ws\\*", dir);
+    HANDLE hFind = FindFirstFileW(szFindCommand, &findData);
+    if (hFind == INVALID_HANDLE_VALUE) return fInfo;
+    do {
+        if (!lstrcmpW(findData.cFileName, L".") || !lstrcmpW(findData.cFileName, L"..")) continue;
+        fInfo.push_back({ findData.cFileName, findData.nFileSizeLow });
+        if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            WCHAR dfs[MAX_PATH];
+            swprintf(dfs, MAX_PATH, L"%ws\\%ws", dir, findData.cFileName);
+            vector<FILEINFO> ret = IndexerWorker(dfs);
+            fInfo.insert(fInfo.end(), ret.begin(), ret.end());
+        }
+    } while (FindNextFileW(hFind, &findData));
+    FindClose(hFind);
+    return fInfo;
 }
 
 DWORD WINAPI Indexer(LPVOID lpParameter) {
-    LPWSTR volumeLetter = (LPWSTR)lpParameter;
+    DRIVE* d = (DRIVE*)lpParameter;
+    vector<FILEINFO> fInfo = IndexerWorker(d->letter);
+    sort(fInfo.begin(), fInfo.end());
+    d->files.clear();
+    d->files.insert(d->files.end(), fInfo.begin(), fInfo.end());
     return 0;
 }
 
@@ -225,17 +286,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         PDEV_BROADCAST_HDR lpdb = (PDEV_BROADCAST_HDR)lParam;
         PDEV_BROADCAST_VOLUME lpdbv = (PDEV_BROADCAST_VOLUME)lpdb;
         WCHAR volumeLetter[] = L"C:";
-        DWORD unitmask = lpdbv->dbcv_unitmask;
         switch (wParam)
         {
         case DBT_DEVICEARRIVAL: {
-            for (char i = 0; i < 26; ++i) {
+            DWORD unitmask = lpdbv->dbcv_unitmask;
+            for (char i = 0; i < 26; ++i, unitmask >>= 1) {
                 if (!(unitmask & 0x1)) continue;
                 volumeLetter[0] = i + 'A';
-                WCHAR volumeName[MAX_PATH_LENGTH] = L"";
+                WCHAR volumeName[MAX_PATH] = L"";
                 DWORD serialNumber = 0;
-                GetVolumeInformation(volumeLetter, volumeName, MAX_PATH_LENGTH, &serialNumber, nullptr, nullptr, nullptr, 0);
-                fwprintf(fpLogfile, L"[%ws]Drive arrival, VolumeLetter=\"%ws\" Serial=%d, VolumeName=\"%ws\"\n", CurTime(), volumeLetter, serialNumber, volumeName);
+                GetVolumeInformation(volumeLetter, volumeName, MAX_PATH, &serialNumber, nullptr, nullptr, nullptr, 0);
                 DRIVE* p = driveHead.pnext;
                 while (p && p->uuid != serialNumber) p = p->pnext;
                 if (!p) {
@@ -244,18 +304,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 }
                 else {
                     if (p->path[0] == L'\0') continue;
-                    CreateThread(nullptr, 0, Indexer, volumeLetter, 0, nullptr);
+                    lstrcpyW(p->letter, volumeLetter);
+                    CreateThread(nullptr, 0, Indexer, p, 0, nullptr);
                 }
-                unitmask >>= 1;
-            }
-            break;
-        }
-        case DBT_DEVICEREMOVECOMPLETE: {
-            for (char i = 0; i < 26; ++i) {
-                if (!(unitmask & 0x1)) continue;
-                volumeLetter[0] = i + 'A';
-                fwprintf(fpLogfile, L"[%ws]Drive remove, VolumeLetter=\"%ws\"\n", CurTime(), volumeLetter);
-                unitmask >>= 1;
             }
             break;
         }
