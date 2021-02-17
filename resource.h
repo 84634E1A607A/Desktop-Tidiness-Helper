c5c501b3942fabab89909981d035656459961079
LPYTY <61695629+LPYTY@users.noreply.github.com>
2021-02-17 10:01:50 +0800

    add popup menu & auto copy
    * works better
    
    * works really fine
    
    * Revert "works really fine"
    
    This reverts commit 1ac111ba44718ce62f314e835f9435c497129b0d.
    
    * works really fine
    
    * Update Desktop Tidiness Helper.cpp
    
    * add popup menu & auto copy

--- resource.h
+++ resource.h
@@ -1,16 +1,21 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Desktop Tidiness Helper.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Desktop Tidiness Helper.rc 使用
 //
 #define IDI_ICON1                       101
 #define TRAY_ICON                       101
+#define IDR_MENU1                       102
+#define ID_X_VIEWLOG                    40001
+#define ID_X_VIEWCONFIG                 40002
+#define ID_X_EXIT                       40003
+#define ID_X_EDITCONFIG                 40004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40005
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
